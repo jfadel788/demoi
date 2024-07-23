99a5e30cd1a5cf0edc77e797970a789a3a442034
jawad <jfadel788@gmail.com>
2024-07-23 14:12:28 +0300

    using dd instad of return view in home route and fix about

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dd('welcome');
 });
-Route::get("/abut",function(){
+Route::get("/about",function(){
     dd("about pages");
 });
 
